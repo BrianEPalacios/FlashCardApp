51615ced2d7604aac16902a758780eaa3e2ea00d
Brian Palacios <b.estpalacios@gmail.com>
2023-02-04 00:15:22 -0600

    initial commit

---
+++ flashcard.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+
+class Flashcard:
+    def __init__(self):
+        # Creating word
+        self.french_word = None
+        self.english_word = None
+        #
+        self.canvas = tk.Canvas(width=800, height=526)
+        # Create images of card
+        self.front_of_card_img = tk.PhotoImage(file="images/card_front.png")
+        self.back_of_card_img = tk.PhotoImage(file="images/card_back.png")
+        # creating background of card
+        self.canvas.create_image(400, 263, image=self.front_of_card_img)
+        self.canvas.create_text(400, 150, text="Hello", font=("Ariel", 40, "italic"))
+        self.canvas.create_text(400, 263, text=f"press any button to begin", font=("Ariel", 60, "bold"))
+        self.canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+        self.canvas.grid(row=0, column=0, columnspan=2)
+
+    def flip_card(self):
+        pass
+
+    def front_of_card(self, french_word, english_word):
+        self.french_word = french_word
+        self.english_word = english_word
+        self.canvas.create_image(400, 263, image=self.front_of_card_img)
+        self.canvas.create_text(400, 150, text="French", font=("Ariel", 40, "italic"))
+        self.canvas.create_text(400, 263, text=f"{self.french_word}", font=("Ariel", 60, "bold"))
+        # self.canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+        # self.canvas.grid(row=0, column=0, columnspan=2)
+
+    def back_of_card(self):
+        self.canvas.create_image(400, 263, image=self.back_of_card_img)
+        self.canvas.create_text(400, 150, text="English", font=("Ariel", 40, "italic"))
+        self.canvas.create_text(400, 263, text=f"{self.english_word}", font=("Ariel", 60, "bold"))
+        # self.canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+        # self.canvas.grid(row=0, column=0, columnspan=2)
---
+++ main.py
@@ -0,0 +1,59 @@
+import tkinter as tk
+from flashcard import Flashcard
+import random
+import pandas as pd
+
+timer = None
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+# ------------------------------ create word list and generate random word -----------------------------------==#
+word_df = pd.read_csv("data/french_words.csv")
+word_dict = word_df.to_dict(orient="records")
+
+
+def generate_random_word():
+    global word_dict
+    current_card = random.choice(word_dict)
+    french_word = current_card["French"]
+    english_word = current_card["English"]
+    return french_word, english_word
+
+
+# ---------------------------------- Button Functionality --------------------------------------------#
+def button_flip_card():
+    flashcard.back_of_card()
+
+
+def x_button_click():
+    global timer
+    french_word, english_word = generate_random_word()
+    flashcard.front_of_card(french_word=french_word, english_word=english_word)
+    timer = window.after(3000, func=button_flip_card)
+
+
+def checkmark_button_click():
+    global timer
+    french_word, english_word = generate_random_word()
+    flashcard.front_of_card(french_word=french_word, english_word=english_word)
+    timer = window.after(3000, func=button_flip_card)
+
+
+# --------------------------- UI Setup --------------------------------------------------------------#
+window = tk.Tk()
+window.title("Flashcard")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+flashcard = Flashcard()
+
+
+# create buttons
+x_image = tk.PhotoImage(file="images/wrong.png")
+x_button = tk.Button(image=x_image, highlightthickness=0, command=x_button_click)
+x_button.grid(row=1, column=0)
+
+checkmark_img = tk.PhotoImage(file="images/right.png")
+checkmark_button = tk.Button(image=checkmark_img, highlightthickness=0, command=checkmark_button_click)
+checkmark_button.grid(row=1, column=1)
+
+window.mainloop()
